74090063a7df56537c726bec075929838207a11f
dodumu <davidrobertq2@gmail.com>
2026-06-23 02:40:52 +0100

    adding css

--- main.go
+++ main.go
@@ -8,15 +8,14 @@ import (
 )
 
 func main() {
-
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	fmt.Println("server is running on http://localhost:8095")
 
 	err := web.SaveAllData()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/", web.HomeHandler)
 	http.HandleFunc("/artist/", web.ArtistHandler)
